005ebd368ad9c7d0ea44c388c90145891b0b5096
pheiselmann <prhmann@gmail.com>
2017-08-14 22:32:27 -0700

    set up test and Travis CI

---
+++ test/test-server.js
@@ -0,0 +1,49 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+// var server = require('../server.js');
+
+//const {app} = require('../server.js');
+
+const {app, runServer, closeServer} = require('../server.js');
+
+
+const should = chai.should();
+// var app = server.app;
+//var storage = server.storage;
+
+chai.use(chaiHttp);
+
+
+describe('index page', function() {
+
+//**
+
+// Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the that promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
+
+  // although we only have one test module at the moment, we'll
+  // close our server at the end of these tests. Otherwise,
+  // if we add another test module that also has a `before` block
+  // that starts our server, it will cause an error because the
+  // server would still be running from the previous tests.
+  after(function() {
+    return closeServer();
+  });
+//**
+
+
+  it('exists', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.html;
+    });
+  });
+});
