8d80ccf10e5400b195829f1ab17e911ad0603116
pheiselmann <prhmann@gmail.com>
2017-08-16 23:36:07 -0700

    add mock data for account page

---
+++ public/account.js
@@ -0,0 +1,42 @@
+// this is mock data, but when we create our API
+// we'll have it return data that looks like this
+var MOCK_USER_INFO = {
+	"name": "Gene Geneticist",
+	"username": "genemachine",
+	"password": "luckyone",
+	"snpVariant": "AA"
+    //“report”: “homozygous dominant for rs1801131”
+};
+// this function's name and argument can stay the
+// same after we have a live API, but its internal
+// implementation will change. Instead of using a
+// timeout function that returns mock data, it will
+// use jQuery's AJAX functionality to make a call
+// to the server and then run the callbackFn
+function getUserInfo(callbackFn) {
+    // we use a `setTimeout` to make this asynchronous
+    // as it would be with a real AJAX call.
+	setTimeout(function(){ callbackFn(MOCK_USER_INFO)}, 100);
+}
+
+// this function stays the same when we connect
+// to real API later
+function displayUserAccountInfo(data) {
+    $('body').append(
+    	'<p>' + 'Name: ' + data.name + '</p>' +
+    	'<p>' + 'username: ' + data.username + '</p>' +
+    	'<p>' + 'password: ' + data.password + '</p>' +
+    	'<p>' + 'snpVariant: ' + data.snpVariant + '</p>');
+}
+
+
+// this function can stay the same even when we
+// are connecting to real API
+function getAndDisplayStatusUpdates() {
+	getRecentStatusUpdates(displayStatusUpdates);
+}
+
+//  on page load do this
+$(function() {
+	getAndDisplayStatusUpdates();
+})
