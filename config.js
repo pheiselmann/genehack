19a5806a5900adb254513fe860f422bfd8ade14d
pheiselmann <prhmann@gmail.com>
2017-08-18 22:00:24 -0700

    change test db to local

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/genehackDb';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
-	'mongodb://localhost/test-genehackDb');
+	//'mongodb://localhost/test-genehackDb');
+	'mongodb://localhost/genehackDb');
 exports.PORT = process.env.PORT || 8080;
