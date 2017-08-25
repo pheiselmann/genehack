ebd9b3042561a138d2326ae64b92ddf11da2491d
pheiselmann <prhmann@gmail.com>
2017-08-24 23:37:52 -0700

    create schema for users with bcrypt validation/password hashing methods

---
+++ users/models.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+  name: {
+    firstName: String,
+    lastName: String
+  },
+  username: {
+    type: String, 
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: true
+  },
+  snpVariant: {type: String}
+  //report: {type: String}
+});
+
+UserSchema.virtual('fullName').get(function() {
+  return `${this.name.firstName} ${this.name.lastName}`.trim();
+});
+
+UserSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    name: this.fullName,
+    username: this.username,
+    snpVariant: this.snpVariant
+    //report: this.report
+  };
+}
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+UserSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
