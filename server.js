ba1d64fe506c4b576e79ce44c1b966a1fc33eac5
Mark Nyon <mark@grandkru.com>
2017-09-12 17:38:19 -0400

    Added mustache as a template engine

--- server.js
+++ server.js
@@ -43,6 +43,15 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+// Adding mustache as a view engine
+var expmustache = require('mustache-express');
+app.engine('mustache', expmustache());
+app.set('view engine','html');
+// Set views directory
+app.set('views', __dirname + './views');
+
+
 app.use(passport.initialize());
 passport.use(jwtStrategy);
 
@@ -74,28 +83,28 @@ function handleRedirects(req, res, next) {
 
 app.use(handleRedirects);
 
-// app.get('/api/protected',
-//     passport.authenticate('jwt', {session: false}),
-//     (req, res) => {
-//       User
-//         .findOne({username: req.body.username})
-//         .exec()
-//     //apiRepr can be used as a token showing someone has logged in
-//     .then(profile => { return res.json(profile.apiRepr())})
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({error: 'something went terribly wrong'});
-//     })
-//   }
-// );
+app.get('/api/protected',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+      User
+        .findOne({username: req.body.username})
+        .exec()
+    //apiRepr can be used as a token showing someone has logged in
+    .then(profile => { return res.json(profile.apiRepr())})
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'something went terribly wrong'});
+    })
+  }
+);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.render('index.html');
 });
 
 
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html');
+  res.render('login.html');
 });
 
 
@@ -105,90 +114,7 @@ app.get('/account',passport.authenticate('jwt', { session: false }), (req, res) 
 
 
 app.get('/review', (req, res) => {
-  res.sendFile(__dirname + '/public/review.html');
-});
-
-
-
-//**
-
-// app.get('/profiles', (req, res) => {
-//   User
-//     .find()
-//     .exec()
-//     .then(profiles => {
-//       res.json(profiles.map(profile => profile.apiRepr()));
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({error: 'something went terribly wrong'});
-//     });
-// });
-
-//****IN PROGRESS
-
-// app.get('/profiles', (req, res) => {
-//     const filters = {};
-//     const queryableFields = ['username', 'password'];
-//     queryableFields.forEach(field => {
-//         if (req.query[field]) {
-//             filters[field] = req.query[field];
-//         }
-//     });
-//     User
-//         .find(filters)
-//         .exec()
-//         .then(Users => res.json(
-//             Users.map(user => user.apiRepr())
-//         ))
-//         .catch(err => {
-//             console.error(err);
-//             res.status(500).json({message: 'Internal server error'})
-//         });
-// });
-
-//****SEE ABOVE
-
-// app.get('/profile', (req, res) => {
-//   User
-//     // .findOne({username: "genemachine", password: "luckyone"})
-//     // .findOne({username: "bobcat", password: "youruncle"})
-//     .findOne({_username: req.username, _password: req.password})
-//     // .findOne({username: req.query.username, password: req.query.password})
-//     .exec()
-//     .then(profile => {res.json(profile.apiRepr())})
-//     .catch(err => {
-//       console.error(err);
-//       res.status(500).json({error: 'something went terribly wrong'});
-//     });
-// });
-
-
-app.post('/login', (req, res) =>  {
-  console.log("POST body = ", req.body);
-  var name, password;
-  if(req.body.username && req.body.password){
-    name = req.body.username;
-    password = req.body.password;
-  }
-  console.log("Name = '" + name + "'") ;
-  console.log("Password = '" + password + "'");
-
-  let user = User
-    .findOne({username: name, password: password})
-    .exec()
-    .then(user => {
-      var payload = {id: user.id};
-      const authToken = createAuthToken(user.apiRepr());
-      res.json({message: "ok", token: authToken});
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-  });
-  if( ! user ){
-    res.status(401).json({message:"no such user found"});
-  }
+  res.render('review.html');
 });
 
 
